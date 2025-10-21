5cccadbf136d82c726667956893ee3d9e25a4889
sevenkieran <sevenkieran@gmail.com>
2025-10-21 19:28:30 +0000

    Added hello world

---
+++ Lab4/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 21, 2025
+ *      Author: vboxuser
+ */
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl;
+	return 0;
+}
